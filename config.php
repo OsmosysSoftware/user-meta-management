f1dca23eaf3d2090f56c09b4b2ee313803681c34
siddhartha <siddharth@OSSLIN004>
2015-11-25 11:54:42 +0530

    fixed the code indentation properly

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 <?php
+
 // Defining constant for the template directory.
-define('TEMPLATE',__DIR__. '/template/');
+define('TEMPLATE', __DIR__ . '/template/');
+
 // Defining constant for the plugin directory.
-define('USER_META_PLUGIN_URL',		plugin_dir_url(__FILE__));
+define('USER_META_PLUGIN_URL', plugin_dir_url(__FILE__));
+
 // Defining constant for the keys to be omitted.
-define ("EXCEMPTED_LIST", serialize (array ('session_tokens',	'mylogin_capabilities','managenav-menuscolumnshidden','metaboxhidden_nav-menus','mylogin_dashboard_quick_press_last_post_id')));
+define("EXCEMPTED_LIST", serialize(array('session_tokens', 'mylogin_capabilities', 'managenav-menuscolumnshidden', 'metaboxhidden_nav-menus', 'mylogin_dashboard_quick_press_last_post_id')));
 
